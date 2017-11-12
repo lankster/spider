aa48dd9c34413b2611440c078be8ca5f84b46793
Eric Morono <tactical92fs@gmail.com>
2017-11-12 12:28:57 -0500

    Added standard styling

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('hello from get handler!')
+  res.send('hello from get handler!')
 })
 
 const appPort = process.env.PORT || 3000
-app.listen(appPort, ()=>{
-    console.log(`Listening on port ${appPort}!`)
+
+app.listen(appPort, () => {
+  console.log(`Listening on port ${appPort}!`)
 })
