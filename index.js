215e0490942856cd4e383048eaf41064f33752e2
Eric Morono <tactical92fs@gmail.com>
2017-11-12 14:07:25 -0500

    Added stuff to index.js

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ app.get('/', (req, res) => {
 app.set('port', process.env.PORT || 3000)
 
 app.listen(app.get('port'), () => {
-  console.log(`Listening on port ${appPort}!`)
+  console.log(`Listening on port ${app.get('port')}!`)
 })
